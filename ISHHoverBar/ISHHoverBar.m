0bce0fd286681ce91318c42c83b32280e7472354
Hal Mueller <hal@mobilegeographics.com>
2019-09-19 15:16:30 -0700

    Revert "Starting on Dark Mode."
    This reverts commit 9fc31512559e808c558cbd04aa9b9eeb2efcd722.

--- ISHHoverBar/ISHHoverBar.m
+++ ISHHoverBar/ISHHoverBar.m
@@ -24,12 +24,7 @@ const CGFloat ISHHoverBarDefaultItemDimension = 44.0;
     CAShapeLayer *masklayer = [CAShapeLayer new];
     self.maskShapeLayer = masklayer;
     masklayer.fillRule = kCAFillRuleEvenOdd;
-    if (@available(iOS 13, *)) {
-        masklayer.fillColor = [[UIColor systemFillColor] CGColor];
-    }
-    else {
-        masklayer.fillColor = [[UIColor blackColor] CGColor];
-    }
+    masklayer.fillColor = [[UIColor blackColor] CGColor];
     self.mask = masklayer;
     self.shadowOffset = CGSizeZero;
     return self;
@@ -77,13 +72,7 @@ const CGFloat ISHHoverBarDefaultItemDimension = 44.0;
 
 - (instancetype)init {
     self = [super init];
-    if (@available(iOS 13, *)) {
-        self.backgroundColor = [UIColor clearColor];
-    }
-    else
-    {
-        self.backgroundColor = [UIColor clearColor];
-    }
+    self.backgroundColor = [UIColor clearColor];
     return self;
 }
 
@@ -218,7 +207,7 @@ const CGFloat ISHHoverBarDefaultItemDimension = 44.0;
     [self addSubview:sepView];
 
     // set default values
-    [self setBorderColor:[UIColor greenColor]];
+    [self setBorderColor:[UIColor lightGrayColor]];
     [self setBorderWidth:1.0 / [[UIScreen mainScreen] scale]];
     [self setCornerRadius:8.0];
     [self setShadowOpacity:0.25];
